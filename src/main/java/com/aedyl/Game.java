b3ef9b6cc9c6560c1fbc8a7f7659b47b5ede94f2
Julien Zanon <julien.zanon@gmail.com>
2020-09-19 17:37:55 +0200

    Add JMH benchmark

---
+++ src/main/java/com/aedyl/Game.java
@@ -0,0 +1,62 @@
+package com.aedyl;
+
+import com.aedyl.domain.Arena;
+import com.aedyl.domain.combat.Round;
+import com.aedyl.domain.fighter.Characteristics;
+import com.aedyl.domain.fighter.CharacteristicsSupplier;
+import com.aedyl.domain.fighter.Human;
+import com.aedyl.domain.fighter.HumanSupplier;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Game {
+	private static final Logger logger = LoggerFactory.getLogger(Game.class);
+
+	@Benchmark
+	public void launch() {
+		int numberOfFighter = 100;
+
+		Arena arena = setupArena(numberOfFighter);
+
+		//arena.getSurvivors().forEach(fighter -> logger.info("Fighter: {}", fighter));
+
+		arena.fight(15);
+
+	//	logResults(arena);
+	}
+
+	private void logResults(Arena arena) {
+		final List<Round> rounds = arena.getRounds();
+
+		rounds.stream()
+				.map(Round::buildSummary)
+				.forEach(logger::info);
+
+		final List<String> winners = arena.getSurvivors().stream()
+				.map(human -> human.name)
+				.collect(Collectors.toList());
+		if (winners.size() == 1) {
+			logger.info("At then end of round {}, the winner is: {}", rounds.size(), winners);
+		} else {
+			logger.info("At then end of round {}, there are {} survivors: {}", rounds.size(), winners.size(), winners);
+		}
+	}
+
+
+	private Arena setupArena(int numberOfFighter) {
+		Supplier<Characteristics> charactSupplier = new CharacteristicsSupplier();
+		Supplier<Human> humanSupplier = new HumanSupplier(charactSupplier);
+
+		List<Human> fighters = IntStream.range(0, numberOfFighter)
+				.mapToObj(value -> humanSupplier.get())
+				.collect(Collectors.toUnmodifiableList());
+
+		return new Arena(fighters);
+	}
+}
--- src/main/java/com/aedyl/Main.java
+++ src/main/java/com/aedyl/Main.java
@@ -1,53 +1,11 @@
 package com.aedyl;
 
-import com.aedyl.domain.Arena;
-import com.aedyl.domain.fighter.Characteristics;
-import com.aedyl.domain.fighter.CharacteristicsSupplier;
-import com.aedyl.domain.fighter.Human;
-import com.aedyl.domain.fighter.HumanSupplier;
-import com.aedyl.domain.combat.Round;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class Main {
-
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
 
 	public static void main(String[] args) {
-
-		Supplier<Characteristics> charactSupplier = new CharacteristicsSupplier();
-		Supplier<Human> humanSupplier = new HumanSupplier(charactSupplier);
-		int numberOfFighter = 2_000;
-
-		List<Human> fighters = IntStream.range(0, numberOfFighter)
-				.mapToObj(value -> humanSupplier.get())
-				.peek(fighter -> logger.info("Fighter: {}", fighter))
-				.collect(Collectors.toUnmodifiableList());
-
-		Arena arena = new Arena(fighters);
-
-		arena.fight(15);
-
-		final List<Round> rounds = arena.getRounds();
-
-		rounds.stream()
-				.map(Round::buildSummary)
-				.forEach(logger::info);
-
-		final List<String> winners = arena.getSurvivors().stream()
-				.map(human -> human.name)
-				.collect(Collectors.toList());
-		if (winners.size() == 1) {
-			logger.info("At then end of round {}, the winner is: {}", rounds.size(), winners);
-		} else {
-			logger.info("At then end of round {}, there are {} survivors: {}", rounds.size(), winners.size(), winners);
-		}
+		Game game = new Game();
+		game.launch();
 
 	}
 
